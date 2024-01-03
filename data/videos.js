904f681c1fc3554c867763a6ebd7e6b9059c0895
Vidukln <59608320+Vidukln@users.noreply.github.com>
2024-01-04 00:57:12 +0530

    Create videos.js

---
+++ data/videos.js
@@ -0,0 +1,498 @@
+const video = [
+  {
+    movieID: "659022d3fac1ad496b92cd61",
+    videoID: "1jtgBG-0YOPGNMrbaE3qzub9edQFdySx8",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd62",
+    videoID: "1Gbjpru0adzT5dvkpebC457l4j1hJzbZj",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd63",
+    videoID: "1CQJSwK4COJxbfMuUePRl9Kupnc0-dif3",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd64",
+    videoID: "1i6NVJBTr8ig1hMJSDZcLV4vpc8ZFdkLh",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd65",
+    videoID: "182_HB_L4d4WyyFDeejdO5plwsVvjKu2o",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd66",
+    videoID: "1LgCxbU7rUZiHzywFejLjs9UI6Nm1gUt5",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd67",
+    videoID: "1-LvlXrtHt3XwS-HvhRwnjbzPPJNms0DI",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd68",
+    videoID: "1P0BVVNBWwAFUIEgloGbt1BH8X6vn2AnK",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd69",
+    videoID: "1bP2JrSKq4D0LORsiqZ9x2DSUulvzrKgQ",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd6a",
+    videoID: "1uO9Fg_5324ajXPRnnG1ctcLngvkYMEQ4",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd6b",
+    videoID: "1NdbpGujBKtw1qkqYuGSZb-slsoejA3g8",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd6c",
+    videoID: "1JOqVAOnca7wVD4ybylNaAdbiE5UTFkwF",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd6d",
+    videoID: "1W5hkdpYSqJ9gc5BbksP_-95hV2KQKZ9y",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd6e",
+    videoID: "1Zs647P3JzFjh1L_HGatKtGPeGRuW-Kke",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd6f",
+    videoID: "1Nre7ZE4WPTIoSuDlNMYVYTks3-eMS_YN",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd70",
+    videoID: "175tZC1jFapeNzGTgg7vGGeYJWLT3gEsb",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd71",
+    videoID: "1a3qTmye8g2zdCURN-kV7SjerGmSpBshp",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd72",
+    videoID: "1UNpLQIURSkbzxl63-jYaQfYFLqnZvZRe",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd73",
+    videoID: "1ePAPIuKhg9xgPknuczog4_YiU1jHswH8",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd74",
+    videoID: "1zWlnMVbIiixkYiCUZcwlWNlrjM0qjhgw",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd75",
+    videoID: "1jgPOQpL_RiSV7XIN2oSgJNi5IUFVYRu9",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd76",
+    videoID: "1M0dVGdyMc0W4LVcrF8r478p3HsTys0Fq",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd77",
+    videoID: "1jhTnmNY0ZS1orp82g3gblqyWxc8MX-1p",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd78",
+    videoID: "1eO4GvKcqDaR-T13ceSNGmxg38HTTPh9v",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd79",
+    videoID: "1bvhxAxIvpiYzEyzqDVMs8Fq52NZNLtuv",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd7a",
+    videoID: "1vdSHv8t1J3AApcV-CKJV8bgworBKV_Ul",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd7b",
+    videoID: "1IfO5mECp_rMaMMjuUVxsRwmtcsbpt6SF",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd7c",
+    videoID: "12krmNDn4EQdTBAKBnGNWDabfqwZTqn29",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd7d",
+    videoID: "12krmNDn4EQdTBAKBnGNWDabfqwZTqn29",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd7e",
+    videoID: "1Zsm5OypzwnBkgU5-vJjnQwP077DYu08X",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd7f",
+    videoID: "1jtgBG-0YOPGNMrbaE3qzub9edQFdySx8",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd80",
+    videoID: "182_HB_L4d4WyyFDeejdO5plwsVvjKu2o",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd81",
+    videoID: "1NdbpGujBKtw1qkqYuGSZb-slsoejA3g8",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd82",
+    videoID: "1uO9Fg_5324ajXPRnnG1ctcLngvkYMEQ4",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd83",
+    videoID: "1W5hkdpYSqJ9gc5BbksP_-95hV2KQKZ9y",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd84",
+    videoID: "1CQJSwK4COJxbfMuUePRl9Kupnc0-dif3",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd85",
+    videoID: "182_HB_L4d4WyyFDeejdO5plwsVvjKu2o",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd86",
+    videoID: "1Nre7ZE4WPTIoSuDlNMYVYTks3-eMS_YN",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd87",
+    videoID: "1LgCxbU7rUZiHzywFejLjs9UI6Nm1gUt5",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd88",
+    videoID: "1W5hkdpYSqJ9gc5BbksP_-95hV2KQKZ9y",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd89",
+    videoID: "175tZC1jFapeNzGTgg7vGGeYJWLT3gEsb",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd8a",
+    videoID: "1NdbpGujBKtw1qkqYuGSZb-slsoejA3g8",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd8b",
+    videoID: "1uO9Fg_5324ajXPRnnG1ctcLngvkYMEQ4",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd8c",
+    videoID: "1LgCxbU7rUZiHzywFejLjs9UI6Nm1gUt5",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd8d",
+    videoID: "1W5hkdpYSqJ9gc5BbksP_-95hV2KQKZ9y",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd8e",
+    videoID: "1W5hkdpYSqJ9gc5BbksP_-95hV2KQKZ9y",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd8f",
+    videoID: "1uO9Fg_5324ajXPRnnG1ctcLngvkYMEQ4",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd90",
+    videoID: "1Zs647P3JzFjh1L_HGatKtGPeGRuW-Kke",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd91",
+    videoID: "1a3qTmye8g2zdCURN-kV7SjerGmSpBshp",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd92",
+    videoID: "1Nre7ZE4WPTIoSuDlNMYVYTks3-eMS_YN",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd93",
+    videoID: "175tZC1jFapeNzGTgg7vGGeYJWLT3gEsb",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd94",
+    videoID: "1W5hkdpYSqJ9gc5BbksP_-95hV2KQKZ9y",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd95",
+    videoID: "1Nre7ZE4WPTIoSuDlNMYVYTks3-eMS_YN",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd96",
+    videoID: "1-LvlXrtHt3XwS-HvhRwnjbzPPJNms0DI",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd97",
+    videoID: "1Zs647P3JzFjh1L_HGatKtGPeGRuW-Kke",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd98",
+    videoID: "1jtgBG-0YOPGNMrbaE3qzub9edQFdySx8",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd99",
+    videoID: "1Nre7ZE4WPTIoSuDlNMYVYTks3-eMS_YN",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd9a",
+    videoID: "1LgCxbU7rUZiHzywFejLjs9UI6Nm1gUt5",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd9b",
+    videoID: "1-LvlXrtHt3XwS-HvhRwnjbzPPJNms0DI",
+  },
+  {
+    movieID: "659022d3fac1ad496b92cd9c",
+    videoID: "1CQJSwK4COJxbfMuUePRl9Kupnc0-dif3",
+  },
+
+  {
+    movieID: "65903e364371516c874aa908",
+    videoID: "1jtgBG-0YOPGNMrbaE3qzub9edQFdySx8",
+  },
+  {
+    movieID: "65903e364371516c874aa909",
+    videoID: "1Gbjpru0adzT5dvkpebC457l4j1hJzbZj",
+  },
+  {
+    movieID: "65903e364371516c874aa90a",
+    videoID: "1CQJSwK4COJxbfMuUePRl9Kupnc0-dif3",
+  },
+  {
+    movieID: "65903e364371516c874aa90b",
+    videoID: "1i6NVJBTr8ig1hMJSDZcLV4vpc8ZFdkLh",
+  },
+  {
+    movieID: "65903e364371516c874aa90c",
+    videoID: "1CQJSwK4COJxbfMuUePRl9Kupnc0-dif3",
+  },
+  {
+    movieID: "65903e364371516c874aa90d",
+    videoID: "1LgCxbU7rUZiHzywFejLjs9UI6Nm1gUt5",
+  },
+  {
+    movieID: "65903e364371516c874aa90e",
+    videoID: "1-LvlXrtHt3XwS-HvhRwnjbzPPJNms0DI",
+  },
+  {
+    movieID: "65903e364371516c874aa90f",
+    videoID: "1P0BVVNBWwAFUIEgloGbt1BH8X6vn2AnK",
+  },
+  {
+    movieID: "65903e364371516c874aa910",
+    videoID: "1bP2JrSKq4D0LORsiqZ9x2DSUulvzrKgQ",
+  },
+  {
+    movieID: "65903e364371516c874aa911",
+    videoID: "1CQJSwK4COJxbfMuUePRl9Kupnc0-dif3",
+  },
+  {
+    movieID: "65903e364371516c874aa912",
+    videoID: "1uO9Fg_5324ajXPRnnG1ctcLngvkYMEQ4",
+  },
+  {
+    movieID: "65903e364371516c874aa913",
+    videoID: "1JOqVAOnca7wVD4ybylNaAdbiE5UTFkwF",
+  },
+  {
+    movieID: "65903e364371516c874aa914",
+    videoID: "1W5hkdpYSqJ9gc5BbksP_-95hV2KQKZ9y",
+  },
+  {
+    movieID: "65903e364371516c874aa915",
+    videoID: "1Zs647P3JzFjh1L_HGatKtGPeGRuW-Kke",
+  },
+  {
+    movieID: "65903e364371516c874aa916",
+    videoID: "1Nre7ZE4WPTIoSuDlNMYVYTks3-eMS_YN",
+  },
+  {
+    movieID: "65903e364371516c874aa917",
+    videoID: "175tZC1jFapeNzGTgg7vGGeYJWLT3gEsb",
+  },
+
+  {
+    movieID: "65903e364371516c874aa918",
+    videoID: "1CQJSwK4COJxbfMuUePRl9Kupnc0-dif3",
+  },
+  {
+    movieID: "65903e364371516c874aa919",
+    videoID: "1a3qTmye8g2zdCURN-kV7SjerGmSpBshp",
+  },
+  {
+    movieID: "65903e364371516c874aa91a",
+    videoID: "1UNpLQIURSkbzxl63-jYaQfYFLqnZvZRe",
+  },
+
+  {
+    movieID: "65903e364371516c874aa91b",
+    videoID: "1ePAPIuKhg9xgPknuczog4_YiU1jHswH8",
+  },
+  {
+    movieID: "65903e364371516c874aa91c",
+    videoID: "1ePAPIuKhg9xgPknuczog4_YiU1jHswH8",
+  },
+  {
+    movieID: "65903e364371516c874aa91d",
+    videoID: "1zWlnMVbIiixkYiCUZcwlWNlrjM0qjhgw",
+  },
+
+  {
+    movieID: "65903e364371516c874aa91e",
+    videoID: "1jgPOQpL_RiSV7XIN2oSgJNi5IUFVYRu9",
+  },
+  {
+    movieID: "65903e364371516c874aa91f",
+    videoID: "1M0dVGdyMc0W4LVcrF8r478p3HsTys0Fq",
+  },
+  {
+    movieID: "65903e364371516c874aa920",
+    videoID: "1eO4GvKcqDaR-T13ceSNGmxg38HTTPh9v",
+  },
+
+  {
+    movieID: "65903e364371516c874aa921",
+    videoID: "1jhTnmNY0ZS1orp82g3gblqyWxc8MX-1p",
+  },
+  {
+    movieID: "65903e364371516c874aa922",
+    videoID: "1CQJSwK4COJxbfMuUePRl9Kupnc0-dif3",
+  },
+
+  {
+    movieID: "65903e364371516c874aa923",
+    videoID: "1f2DA-j5gBvA5dADUYAzXdXxEaSGz4ZwX",
+  },
+  {
+    movieID: "65903e364371516c874aa924",
+    videoID: "1Zsm5OypzwnBkgU5-vJjnQwP077DYu08X",
+  },
+  {
+    movieID: "65903e364371516c874aa925",
+    videoID: "1jtgBG-0YOPGNMrbaE3qzub9edQFdySx8",
+  },
+
+  {
+    movieID: "65903e364371516c874aa926",
+    videoID: "1Gbjpru0adzT5dvkpebC457l4j1hJzbZj",
+  },
+  {
+    movieID: "65903e364371516c874aa927",
+    videoID: "1i6NVJBTr8ig1hMJSDZcLV4vpc8ZFdkLh",
+  },
+  {
+    movieID: "65903e364371516c874aa928",
+    videoID: "182_HB_L4d4WyyFDeejdO5plwsVvjKu2o",
+  },
+
+  {
+    movieID: "65903e364371516c874aa929",
+    videoID: "1i6NVJBTr8ig1hMJSDZcLV4vpc8ZFdkLh",
+  },
+
+  {
+    movieID: "65903e364371516c874aa92a",
+    videoID: "182_HB_L4d4WyyFDeejdO5plwsVvjKu2o",
+  },
+  {
+    movieID: "65903e364371516c874aa92b",
+    videoID: "1LgCxbU7rUZiHzywFejLjs9UI6Nm1gUt5",
+  },
+
+  {
+    movieID: "65903e364371516c874aa92c",
+    videoID: "1-LvlXrtHt3XwS-HvhRwnjbzPPJNms0DI",
+  },
+  {
+    movieID: "65903e364371516c874aa92d",
+    videoID: "1P0BVVNBWwAFUIEgloGbt1BH8X6vn2AnK",
+  },
+
+  {
+    movieID: "65903e364371516c874aa92e",
+    videoID: "1bP2JrSKq4D0LORsiqZ9x2DSUulvzrKgQ",
+  },
+  {
+    movieID: "65903e364371516c874aa92f",
+    videoID: "1uO9Fg_5324ajXPRnnG1ctcLngvkYMEQ4",
+  },
+
+  {
+    movieID: "65903e364371516c874aa930",
+    videoID: "1NdbpGujBKtw1qkqYuGSZb-slsoejA3g8",
+  },
+
+  {
+    movieID: "65903e364371516c874aa931",
+    videoID: "1JOqVAOnca7wVD4ybylNaAdbiE5UTFkwF",
+  },
+  {
+    movieID: "65903e364371516c874aa932",
+    videoID: "1W5hkdpYSqJ9gc5BbksP_-95hV2KQKZ9y",
+  },
+  {
+    movieID: "65903e364371516c874aa933",
+    videoID: "1Zs647P3JzFjh1L_HGatKtGPeGRuW-Kke",
+  },
+  {
+    movieID: "65903e364371516c874aa934",
+    videoID: "1Nre7ZE4WPTIoSuDlNMYVYTks3-eMS_YN",
+  },
+
+  {
+    movieID: "65903e364371516c874aa935",
+    videoID: "175tZC1jFapeNzGTgg7vGGeYJWLT3gEsb",
+  },
+  {
+    movieID: "65903e364371516c874aa936",
+    videoID: "1a3qTmye8g2zdCURN-kV7SjerGmSpBshp",
+  },
+  {
+    movieID: "65903e364371516c874aa937",
+    videoID: "1UNpLQIURSkbzxl63-jYaQfYFLqnZvZRe",
+  },
+  {
+    movieID: "65903e364371516c874aa938",
+    videoID: "1ePAPIuKhg9xgPknuczog4_YiU1jHswH8",
+  },
+  {
+    movieID: "65903e364371516c874aa939",
+    videoID: "1zWlnMVbIiixkYiCUZcwlWNlrjM0qjhgw",
+  },
+  {
+    movieID: "65903e364371516c874aa93a",
+    videoID: "1jgPOQpL_RiSV7XIN2oSgJNi5IUFVYRu9",
+  },
+  {
+    movieID: "65903e364371516c874aa93b",
+    videoID: "1M0dVGdyMc0W4LVcrF8r478p3HsTys0Fq",
+  },
+  {
+    movieID: "65903e364371516c874aa93c",
+    videoID: "1jhTnmNY0ZS1orp82g3gblqyWxc8MX-1p",
+  },
+  {
+    movieID: "65903e364371516c874aa93d",
+    videoID: "1eO4GvKcqDaR-T13ceSNGmxg38HTTPh9v",
+  },
+  {
+    movieID: "65903e364371516c874aa93e",
+    videoID: "1bvhxAxIvpiYzEyzqDVMs8Fq52NZNLtuv",
+  },
+  {
+    movieID: "65903e364371516c874aa93f",
+    videoID: "1vdSHv8t1J3AApcV-CKJV8bgworBKV_Ul",
+  },
+  {
+    movieID: "65903e364371516c874aa940",
+    videoID: "1IfO5mECp_rMaMMjuUVxsRwmtcsbpt6SF",
+  },
+  {
+    movieID: "65903e364371516c874aa941",
+    videoID: "12krmNDn4EQdTBAKBnGNWDabfqwZTqn29",
+  },
+  {
+    movieID: "65903e364371516c874aa942",
+    videoID: "12krmNDn4EQdTBAKBnGNWDabfqwZTqn29",
+  },
+  {
+    movieID: "65903e364371516c874aa943",
+    videoID: "1Zsm5OypzwnBkgU5-vJjnQwP077DYu08X",
+  },
+];
+
+export default video;
